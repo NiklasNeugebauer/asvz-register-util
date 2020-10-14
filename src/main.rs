57795bf36dfa731598d56aad7728f9ca2a2d8c5f
niklasn <niklasneugebauer@outlook.com>
2020-10-14 20:17:57 +0200

    add lesson id and token input

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,28 @@ use hyper::header::AUTHORIZATION;
 use futures::{join, executor};
 
 
+fn str_input(message: &str) -> String {
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    print!("{}", message);
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    return s;
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let request1 = check_available("LESSON", "Bearer TOKEN");
+    
+
+    let lesson_id = str_input("lesson_id: ");
+    let token = str_input("token: ");
+    let request1 = request_enrollment(&lesson_id, &token);
     let res = request1.await;
 
     Ok(())
@@ -37,7 +56,7 @@ async fn request_enrollment(lesson_id: &str, auth_token: &str) -> Result<(), Box
                 None => println!("Success, but no placement. Another thread may have enrolled. Stopping!"),
             };
             
-            //break;
+            break;
         }
         else {
             println!("Status: {}", status);
@@ -50,7 +69,7 @@ async fn request_enrollment(lesson_id: &str, auth_token: &str) -> Result<(), Box
 async fn check_available(lesson_id: &str, auth_token: &str) -> Result<(), Box<dyn std::error::Error>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let uri = format!("https://schalter.asvz.ch/tn-api/api/Lessons/{}??t=0", lesson_id);
+    let uri = format!("https://schalter.asvz.ch/tn-api/api/Lessons/{}??t=1605697821017", lesson_id);
     let req = Request::builder()
         .method(Method::GET)
         .uri(uri)
